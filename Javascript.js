24331a23b3a18228939c247accc18001e8210deb
Stanley Z <szeng135@outlook.com>
2023-03-31 23:51:57 -0400

    initialize html and added functions to Javascript file

---
+++ Javascript.js
@@ -0,0 +1,14 @@
+/* calculator functions */
+let add = function(a,b) {
+    return a + b
+};
+
+let subtract = (a,b) => a-b;
+let multiplication = (a,b) => a*b;
+let division = (a,b) => a/b; 
+
+
+console.log(add(3,6));
+console.log(subtract(3,6));
+console.log(multiplication(3,6));
+console.log(division(3,6));
