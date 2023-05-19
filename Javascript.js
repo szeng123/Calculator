dbb0e75b6717009d080551c5f1d34de52e6070c9
Stanley Z <szeng135@outlook.com>
2023-05-19 09:31:54 -0400

    Modify operator functions for all situations

--- Javascript.js
+++ Javascript.js
@@ -1,6 +1,6 @@
 /* calculator functions */
 let add = function(a,b) {
-    return a + b
+    return a+b
 };
 
 let subtract = (a,b) => a-b;
@@ -11,10 +11,12 @@ let divide = (a,b) => a/b;
 
 let firstNumber = '';
 let secondNumber = '';
-let operator = '';
-let result = '';
+let previousOperator = null;
+let currentOperator = null; 
 
 const operate = function(a,operation,b) {
+    a = Number(a);
+    b = Number(b);
     switch (operation) {
         case '+':
             return add(a,b);
@@ -22,12 +24,15 @@ const operate = function(a,operation,b) {
             return subtract(a,b);
         case '*':
             return multiply(a,b);
-        case '/':
-            return divide(a,b);
-        default:
-            alert('please enter an operator!');
-            break;
+        case '÷':
+            if (b == 0) return null
+            else return divide(a,b);
     };
+};
+
+/*Function to round number of float number*/ 
+function RoundNumber(number) {
+    return Math.round(number * 1000) / 1000;
 };
 
 /* Number and operation functions */
@@ -38,7 +43,7 @@ const upperDisplay = document.getElementById('upperDisplay'); // select upper ca
 
 const clearAction = document.getElementById('clear');
 const deleteAction = document.getElementById('delete');
-const equalsAction = document.getElementById('equalSign');
+
 
 const firstOperand = document.getElementById('firstOperand');
 const displayOperator = document.getElementById('operator');
@@ -54,8 +59,10 @@ for (let button of numberButtons) {  //loop through all numbers
 function appendNumber(number) {
     if (number === '.' && lowerDisplay.textContent.includes('.')) return //do nothing if number already contains decimal
     else if (lowerDisplay.textContent.length > 16) return //do nothing if length of number exceeds digits displayed
-    else if (lowerDisplay.textContent === '0' && number !== '.'){  // changes from zero to digit if number is pressed
+    else if (lowerDisplay.textContent === '0' && number !== '.'){  // changes from zero to decimal number if number is pressed
         lowerDisplay.textContent = number.toString();
+    } else if ((lowerDisplay.textContent == '')&&(number === '.')){
+        lowerDisplay.textContent = '0' + number.toString();
     } else {
     lowerDisplay.textContent = lowerDisplay.textContent.toString() + number.toString();
     }};
@@ -63,12 +70,44 @@ function appendNumber(number) {
 // syntax to determine operator
 const operatorButtons = document.getElementsByClassName('operator-button'); // select operator buttons 
 
-for (let button of operatorButtons) {
-    button.addEventListener('click', e => chooseOperator(e.target.textContent));
+for (let button of operatorButtons) {  //loop through all operator buttons
+    button.addEventListener('click', e => 
+    chooseOperator(e.target.textContent));
 };
 
-function chooseOperator(operator) {
-    if (operator !== null ) operate(); // if operator is declared with first and second operand, calculate result instead
-    firstOperand.textContent = lowerDisplay.textContent;
-    displayOperator.textContent = operator.toString();
+function chooseOperator(operator) { 
+    if (previousOperator !== null) { //if operator is already shown in upper display, get result first 
+        secondNumber = lowerDisplay.textContent; 
+        let newFirstOperand = RoundNumber(operate(firstNumber, previousOperator, secondNumber));
+
+        if (newFirstOperand === null) {
+            alert('Cannot divide by zero!'); // do nothing if value is null (value divided by zero)
+            return;
+        } else {
+            firstOperand.textContent = newFirstOperand;
+            firstNumber = newFirstOperand;
+
+            displayOperator.textContent = operator.toString();
+            previousOperator = displayOperator.textContent;
+        
+            lowerDisplay.textContent = '';
+        };
+    } else {  // only update display with firstOperand and chosen operation
+        firstOperand.textContent = lowerDisplay.textContent;
+        firstNumber = firstOperand.textContent;
+
+        displayOperator.textContent = operator.toString();
+        previousOperator = displayOperator.textContent;
+
+        lowerDisplay.textContent = '';
+    }};
+
+// syntax for evaluate button operation
+const equalsAction = document.getElementById('equalSign');
+
+equalsAction.addEventListener('click', e => compute(e.target.textContent));
+
+function compute(button) {
+    if ((firstOperand.textContent !== '') && (displayOperator.textContent !== '') && (secondOperand.textContent !== '')) return 
+    
 }
