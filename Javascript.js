d470ab986ecbf7a850f209cc3ac234a09043e360
Stanley Z <szeng135@outlook.com>
2023-05-28 23:02:02 -0400

    Completed all calculator functions

--- Javascript.js
+++ Javascript.js
@@ -11,8 +11,7 @@ let divide = (a,b) => a/b;
 
 let firstNumber = '';
 let secondNumber = '';
-let previousOperator = null;
-let currentOperator = null; 
+let chosenOperator = null;
 
 const operate = function(a,operation,b) {
     a = Number(a);
@@ -32,6 +31,7 @@ const operate = function(a,operation,b) {
 
 /*Function to round number of float number*/ 
 function RoundNumber(number) {
+    if (number == null) return null;
     return Math.round(number * 1000) / 1000;
 };
 
@@ -40,10 +40,6 @@ function RoundNumber(number) {
 const buttons = document.querySelector('button'); // select all calculator buttons 
 const lowerDisplay = document.getElementById('lowerDisplay'); // select lower calculator display area
 const upperDisplay = document.getElementById('upperDisplay'); // select upper calculator display area
-
-const clearAction = document.getElementById('clear');
-const deleteAction = document.getElementById('delete');
-
 
 const firstOperand = document.getElementById('firstOperand');
 const displayOperator = document.getElementById('operator');
@@ -76,31 +72,50 @@ for (let button of operatorButtons) {  //loop through all operator buttons
 };
 
 function chooseOperator(operator) { 
-    if (previousOperator !== null) { //if operator is already shown in upper display, get result first 
-        secondNumber = lowerDisplay.textContent; 
-        let newFirstOperand = RoundNumber(operate(firstNumber, previousOperator, secondNumber));
+    if ((chosenOperator !== null) && (lowerDisplay.textContent === '')) { //switches operator if second operand has not been decided yet
+        firstOperand.textContent = lowerDisplay.textContent;
 
-        if (newFirstOperand === null) {
-            alert('Cannot divide by zero!'); // do nothing if value is null (value divided by zero)
+        displayOperator.textContent = operator.toString();
+        chosenOperator = displayOperator.textContent; 
+    }
+    else if ((chosenOperator !== null) && (secondOperand.textContent == '') && (lowerDisplay.textContent.length !==0)) { //if operator is already shown in upper display, perform operations
+        secondNumber = lowerDisplay.textContent;
+        let newFirstOperand = RoundNumber(operate(firstNumber, chosenOperator, secondNumber));
+
+        if ((newFirstOperand == null) && (chosenOperator == '÷')) {
+            alert('Cannot divide by zero!'); // do nothing if value is going to be null (value divided by zero)
             return;
-        } else {
+        } else {  //update first Operand value after performing evaluation 
             firstOperand.textContent = newFirstOperand;
             firstNumber = newFirstOperand;
 
             displayOperator.textContent = operator.toString();
-            previousOperator = displayOperator.textContent;
+            chosenOperator = displayOperator.textContent;
         
+            secondOperand.textContent = '';
+
             lowerDisplay.textContent = '';
         };
-    } else {  // only update display with firstOperand and chosen operation
+    } else if (secondOperand.textContent.includes('=')){   //updates first Operand after evaluation button has been pressed 
         firstOperand.textContent = lowerDisplay.textContent;
         firstNumber = firstOperand.textContent;
 
         displayOperator.textContent = operator.toString();
-        previousOperator = displayOperator.textContent;
+        chosenOperator = displayOperator.textContent; 
+
+        secondOperand.textContent = '';
+        lowerDisplay.textContent = '';
+
+    } else {  // only update display with firstOperand and chosen operation 
+        firstOperand.textContent = lowerDisplay.textContent;
+        firstNumber = firstOperand.textContent;
+
+        displayOperator.textContent = operator.toString();
+        chosenOperator = displayOperator.textContent;
 
         lowerDisplay.textContent = '';
-    }};
+    }
+};
 
 // syntax for evaluate button operation
 const equalsAction = document.getElementById('equalSign');
@@ -108,6 +123,47 @@ const equalsAction = document.getElementById('equalSign');
 equalsAction.addEventListener('click', e => compute(e.target.textContent));
 
 function compute(button) {
-    if ((firstOperand.textContent !== '') && (displayOperator.textContent !== '') && (secondOperand.textContent !== '')) return 
-    
-}
+    if ((firstOperand.textContent !== '') && (displayOperator.textContent !== '') && (!secondOperand.textContent.includes('=')) && (lowerDisplay.textContent !== '')) {  // if firstOperand, operator and secondOperand is decided
+        secondNumber = lowerDisplay.textContent; // update upper display with secondOperand and equals sign
+        
+        let result = RoundNumber(operate(firstNumber, chosenOperator, secondNumber));
+        if ((result == null) && (chosenOperator == '÷')) {
+            alert('Cannot divide by zero!'); // do nothing if value is going to be null (value divided by zero)
+            return;
+        } else {
+        secondOperand.textContent = secondNumber + button.toString(); 
+        lowerDisplay.textContent = result;
+        };
+    }else return  // do nothing otherwise
+};
+
+// Syntax for Clear-all button 
+
+const clearallAction = document.getElementById('clear');
+
+clearallAction.addEventListener('click', e => reset(e.target.textContent));
+
+function reset(button) {
+    firstOperand.textContent = '';
+    firstNumber = '';
+    displayOperator.textContent = '';
+    chosenOperator = null;
+    secondOperand.textContent = '';
+    secondNumber = '';
+    lowerDisplay.textContent = '0';
+};
+
+// Syntax for delete button 
+
+const deleteAction = document.getElementById('delete');
+
+deleteAction.addEventListener('click', e => backspace(e.target.textContent));
+
+function backspace(button) {
+    if (lowerDisplay.textContent == '0'){
+        return // do nothing if the number is already 0 
+    } else if (lowerDisplay.textContent.length == 1){
+        lowerDisplay.textContent = '0';   //revert number to zero on last digit
+    } else 
+    lowerDisplay.textContent = lowerDisplay.textContent.slice(0,-1);
+};
